637818f15fe426c0f016206952c80eae39704d57
anitta <anitta-kasem@hotmail.com>
2021-05-16 19:42:27 +0300

    committed

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Button(){
+
+    return(
+
+
+        <button><span>+</span></button>
+    );
+}
+
+export default Button;
---
+++ src/InputComp.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function InputComp() {
+
+  
+    return (
+      <form>
+  
+        <input
+          name="text"
+          value="Your text here"
+          type="text" />
+      </form>
+    );
+  }
+  
+  export default InputComp;
